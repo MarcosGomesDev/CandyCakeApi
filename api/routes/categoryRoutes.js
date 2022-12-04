03f02b8571b0bfd5f65ecca47eaeabb472434cba
Marcos Gomes <69877839+MarcosGomesDev@users.noreply.github.com>
2022-12-04 02:26:46 -0300

    Update categoryRoutes.js

--- api/routes/categoryRoutes.js
+++ api/routes/categoryRoutes.js
@@ -1,1 +1,7 @@
+import express from 'express';
+import { listCategories } from '../controllers/categories';
+const categoryRoutes = express.Router()
 
+categoryRoutes.get('/categories', listCategories)
+
+module.exports = categoryRoutes
