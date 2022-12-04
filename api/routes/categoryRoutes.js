79ff453c9d5634b25e930366617fd38640f3e9d1
Marcos Gomes <69877839+MarcosGomesDev@users.noreply.github.com>
2022-12-04 02:33:35 -0300

    Delete api directory

--- api/routes/categoryRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { listcategories } from '../controllers/categories';
-const categoryRoutes = express.Router()
-
-categoryRoutes.get('/categories', listcategories)
-
-module.exports = categoryRoutes
--- api/routes/productRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { listProducts } from '../controllers/products';
-const productRoutes = express.Router()
-
-productRoutes.get('/products', listProducts)
-
-module.exports = productRoutes
