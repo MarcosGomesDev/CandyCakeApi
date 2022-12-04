030d494829c7aa8a40dec48a40978607924a45f2
Marcos Gomes <69877839+MarcosGomesDev@users.noreply.github.com>
2022-12-04 01:59:46 -0300

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,8 +1,13 @@
 import express from 'express';
 const router = express.Router()
 
-router.get('/', (res) => {
-    res.json('DENTRO DE PRODUTOS')
+router.get('/', async (req, res) => {
+    try {
+        res.status(200).json('Get products successfully')
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json('server error')
+    }
 })
 
 module.exports = router
