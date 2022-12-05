7c1517cee5d35153777c9175819a52a35f17b391
Marcos Gomes <69877839+MarcosGomesDev@users.noreply.github.com>
2022-12-04 23:43:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const mongoose_1 = __importDefault(require("mongoose"));
 require("dotenv/config");
 const userRoutes_1 = __importDefault(require("./api/routes/userRoutes"));
 const sellerRoutes_1 = __importDefault(require("./api/routes/sellerRoutes"));
 const categoryRoutes_1 = __importDefault(require("./api/routes/categoryRoutes"));
 const subcategoryRoutes_1 = __importDefault(require("./api/routes/subcategoryRoutes"));
 const productRoutes_1 = __importDefault(require("./api/routes/productRoutes"));
-require("./api/database");
+// import './api/database'
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.use((0, cookie_parser_1.default)());
-app.use(userRoutes_1.default);
-app.use(sellerRoutes_1.default);
-app.use(categoryRoutes_1.default);
-app.use(subcategoryRoutes_1.default);
-app.use(productRoutes_1.default);
-app.get('/', (res) => {
-    return res.json('ola mundo');
-});
-app.listen(process.env.PORT, () => {
-    console.log('server on');
-});
+const init = express_1.default.Router();
+const MONGO_URI = "mongodb+srv://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD + "@cluster0.1bqmy.mongodb.net/CandyCake?retryWrites=true&w=majority";
+mongoose_1.default.connect(MONGO_URI)
+    .then(() => {
+    console.log('our db as connect');
+    app.use((0, cors_1.default)());
+    app.use(express_1.default.json());
+    app.use((0, cookie_parser_1.default)());
+    app.use(userRoutes_1.default);
+    app.use(sellerRoutes_1.default);
+    app.use(categoryRoutes_1.default);
+    app.use(subcategoryRoutes_1.default);
+    app.use(productRoutes_1.default);
+    init.get('/', (res) => {
+        return res.status(200).json('Api conectada com sucesso!');
+    });
+    app.listen(process.env.PORT, () => {
+        console.log('server on');
+    });
+})
+    .catch((err) => console.log(err));
